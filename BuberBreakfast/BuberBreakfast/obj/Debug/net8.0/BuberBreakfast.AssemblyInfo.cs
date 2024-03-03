55c0dd3fff2521dc18cd3b991aec4c44559c4ddb
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-03-03 19:33:45 +0200

    build

--- BuberBreakfast/BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346dae20dbbb82a6c8ecafb3bd145c9ea4718f41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8343069353cb477a7b388d0e9df4248c71cb13")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
