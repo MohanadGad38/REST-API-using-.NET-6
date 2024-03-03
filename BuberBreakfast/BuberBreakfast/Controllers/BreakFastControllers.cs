cc1e7dc1560c33bc6329d86fa5eb0c4f5946f28d
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-03-03 19:29:58 +0200

    creating get and post class

--- BuberBreakfast/BuberBreakfast/Controllers/BreakFastControllers.cs
+++ BuberBreakfast/BuberBreakfast/Controllers/BreakFastControllers.cs
@@ -1,8 +1,20 @@
+using BuberBreakfast.Contracts.Breakfast;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakFast.Controllers;
 [ApiController]
 public class BreakfastController: ControllerBase
 {
+    [HttpPost("/breakfast")]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
 
+
+    [HttpGet("/breakfast/{id:guid}")]
+    public IActionResult Breakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
 }
