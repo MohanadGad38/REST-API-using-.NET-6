2672234f1d4bd7ac0fd29c7e3ea7219bc2e8b919
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-03-04 05:35:23 +0200

    done tommrow i will upload video for testing

--- BuberBreakfast/BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.AssemblyInfo.cs
+++ BuberBreakfast/BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9784eaf3619e737276137dfdf48aa1a1fe0c9233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06439c29622ecc23a9c917424f75a6b2228da525")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
